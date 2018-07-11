fd1874295b250456d422ca5bcce8c29d7f8869df
florianPat <florian.patruck@gmx.de>
2018-07-11 16:23:29 +0200

    DroidBlaster_Part4

---
+++ NDKTest/NDKTest.NativeActivity/TimeManager.cpp
@@ -0,0 +1,32 @@
+#include "TimeManager.h"
+#include "Log.h"
+#include "stdlib.h"
+
+TimeManager::TimeManager()
+{
+	srand(time(nullptr));
+	reset();
+}
+
+void TimeManager::reset()
+{
+	utilsLog("Resetting TimeManager");
+	elapsed = 0.0f;
+	firstTime = now();
+	lastTime = firstTime;
+}
+
+void TimeManager::update()
+{
+	double currentTime = now();
+	elapsed = currentTime - lastTime;
+	elapsedTotal = currentTime - firstTime;
+	lastTime = currentTime;
+}
+
+double TimeManager::now()
+{
+	timespec timeVal;
+	clock_gettime(CLOCK_MONOTONIC, &timeVal);
+	return timeVal.tv_sec + (timeVal.tv_nsec * 1.0e-9);
+}
